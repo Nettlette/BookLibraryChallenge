519bbaf6bbb56e8670cbb88e8c785fad91f455f7
Crystal Turner <dances_with_God@yahoo.com>
2024-08-02 15:07:42 -0400

    initial pages and routes

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+//const blogPostRoutes = require("./blogPostRoutes");
+//const commentRoutes = require("./commentRoutes");
+
+// Middleware
+router.use("/users", userRoutes);
+// router.use("/blogposts", blogPostRoutes);
+// router.use("/comments", commentRoutes);
+
+// Exports
+module.exports = router;
